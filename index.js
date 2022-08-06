c883f53e2486389db76371c29ea2482b8ca4feb7
Jessie Callaway <jessie.callaway91@gmail.com>
2022-08-05 19:25:53 -0500

    created a function to dynamically render pokemon cards

--- index.js
+++ index.js
@@ -1,174 +1,199 @@
 // ✅ What is the DOM?
 
-	// Chrome Dev Tools
+// Chrome Dev Tools
 
 // ✅ Creating / Reading DOM Elements
 
-    // .createElement() => creates one DOM element
+// .createElement() => creates one DOM element
 
-	// .querySelector() => returns the first matching DOM element
+// .querySelector() => returns the first matching DOM element
 
-	// .querySelectorAll() => returns an array-like collection of matching DOM elements
+// .querySelectorAll() => returns an array-like collection of matching DOM elements
 
-	// .getElementById() => returns the DOM element with matching Id
+// .getElementById() => returns the DOM element with matching Id
 
-	// .getElementsByClassName() => returns an array-like collection of DOM elements with matching Class
+// .getElementsByClassName() => returns an array-like collection of DOM elements with matching Class
 
-	// .getElementsByTagName() => when is this especially useful?
-        
+// .getElementsByTagName() => when is this especially useful?
+
 // ✅ Updating / Deleting DOM Elements
-        
-	// .append
 
-	// .remove()
+// .append
+
+// .remove()
 
 // ✅ .innerHTML vs. .innerText vs. .textContent
 
-	// let firstDiv = document.getElementById('lecture-goals');
+// let firstDiv = document.getElementById('lecture-goals');
 
-  	// .innerHTML => if you want to insert HTML within a DOM element, pure HTML	
+// .innerHTML => if you want to insert HTML within a DOM element, pure HTML
 
-		// let anotherElement = document.createElement("h1");
-	 
-		// firstDiv.append(anotherElement);
+// let anotherElement = document.createElement("h1");
 
-		// firstDiv.innerHTML = "<h1>Hey!</h1>"
+// firstDiv.append(anotherElement);
 
-	 	// console.log(firstDiv.innerHTML);
+// firstDiv.innerHTML = "<h1>Hey!</h1>"
 
-	// .innerText => if you only want to insert text, includes only human-readable elements
+// console.log(firstDiv.innerHTML);
 
-		// firstDiv.innerText = "Hey!";
+// .innerText => if you only want to insert text, includes only human-readable elements
 
-		// console.log(firstDiv.innerText);
+// firstDiv.innerText = "Hey!";
 
-	// .textContent => if you only want to insert text, includes formatting of elements
+// console.log(firstDiv.innerText);
 
-		// firstDiv.textContent = "Hey!";
+// .textContent => if you only want to insert text, includes formatting of elements
 
-		// console.log(firstDiv.textContent);
+// firstDiv.textContent = "Hey!";
+
+// console.log(firstDiv.textContent);
 
 // -------------------------------------------
 
-console.log("------------------------");
-console.log("⬇️ Break Out Activities ⬇️");
-console.log("🚨 Comment Out Lecture Code Above Before Starting 🚨");
-console.log("💡 Use console.log() To Check Answers 💡");
-console.log("------------------------");
+console.log('------------------------');
+console.log('⬇️ Break Out Activities ⬇️');
+console.log('🚨 Comment Out Lecture Code Above Before Starting 🚨');
+console.log('💡 Use console.log() To Check Answers 💡');
+console.log('------------------------');
 
 // 🚧 Break Out Activity 1: Creating / Reading / Updating DOM Elements
 
-  // 🚨 Comment out any conflicting code above before proceeding.
+// 🚨 Comment out any conflicting code above before proceeding.
 
-		// array of pokemon objects
-		const pokemon = [
-			{
-				id: 1,
-				name: "Bulbasaur",
-				img: "./images/bulbasaur.png",
-				likes: 4,
-			},
-			{
-				id: 2,
-				name: "Ivysaur",
-				img: "./images/ivysaur.png",
-				likes: 21,
-			},
-			{
-				id: 3,
-				name: "Venusaur",
-				img: "./images/venasaur.png",
-				likes: 7,
-			},
-			{
-				id: 4,
-				name: "Charmander",
-				img: "./images/charmander.png",
-				likes: 20,
-			},
-			{
-				id: 5,
-				name: "Charmeleon",
-				img: "./images/charmeleon.png",
-				likes: 11,
-			},
-		];
+// array of pokemon objects
+const pokemon = [
+	{
+		id: 1,
+		name: 'Bulbasaur',
+		img: './images/bulbasaur.png',
+		likes: 4,
+	},
+	{
+		id: 2,
+		name: 'Ivysaur',
+		img: './images/ivysaur.png',
+		likes: 21,
+	},
+	{
+		id: 3,
+		name: 'Venusaur',
+		img: './images/venasaur.png',
+		likes: 7,
+	},
+	{
+		id: 4,
+		name: 'Charmander',
+		img: './images/charmander.png',
+		likes: 20,
+	},
+	{
+		id: 5,
+		name: 'Charmeleon',
+		img: './images/charmeleon.png',
+		likes: 11,
+	},
+];
 
-		// container to store DOM element with ID of "poke-container" => <div id="poke-container"></div>
-		const pokeContainer = document.getElementById("poke-container");
+// container to store DOM element with ID of "poke-container" => <div id="poke-container"></div>
+const pokeContainer = document.getElementById('poke-container');
+console.log('hi');
+// 1️⃣ Given "pokemon" and "pokeContainer" above, create a function (renderPokemon) that:
 
-  // 1️⃣ Given "pokemon" and "pokeContainer" above, create a function (renderPokemon) that:
+//  ✔️ Accepts a single "pokemon" object
 
-	//  ✔️ Accepts a single "pokemon" object
- 
- 	//  ✔️ Creates a "div" element to contain information about each Pokemon and stores the "div" in a variable (pokeCard)
-		//	Assign pokeCard a unique ID with the Pokemon's "id" prepended by "poke-" (i.e., "poke-1" for Bulbasaur)
-		//	Assign pokeCard a className of "poke-card"
-	
-	//  ✔️ Creates an "img" element for each Pokemon and stores the "img" in a variable (pokeImg)
-		//	Assign pokeImg a source (src) linking to the Pokemon's image
-		// 	Assign pokeImg a unique alt with the Pokemon's name followed by " image" (i.e., "bulbasaur image")
-	
-	//	✔️ Creates an "h3" element for each Pokemon and stores the element in a variable (pokeName)
-		// 	Use textContent() to fill in the heading with the Pokemon's name
+//  ✔️ Creates a "div" element to contain information about each Pokemon and stores the "div" in a variable (pokeCard)
+//	Assign pokeCard a unique ID with the Pokemon's "id" prepended by "poke-" (i.e., "poke-1" for Bulbasaur)
+//	Assign pokeCard a className of "poke-card"
 
-	//	✔️ Appends all variables to "pokeCard" before appending "pokeCard" to "pokeContainer"
-		// 	Use append() as necessary
+//  ✔️ Creates an "img" element for each Pokemon and stores the "img" in a variable (pokeImg)
+//	Assign pokeImg a source (src) linking to the Pokemon's image
+// 	Assign pokeImg a unique alt with the Pokemon's name followed by " image" (i.e., "bulbasaur image")
 
-	// 	💡 Use Chrome Dev Tools to view changes being made to the DOM
+//	✔️ Creates an "h3" element for each Pokemon and stores the element in a variable (pokeName)
+// 	Use textContent() to fill in the heading with the Pokemon's name
 
-	// 	💡 Take a look at "styles.css" to see how the DOM Elements we're building out correspond to CSS class and ID selectors.
+//	✔️ Appends all variables to "pokeCard" before appending "pokeCard" to "pokeContainer"
+// 	Use append() as necessary
 
-	// 	✨ BONUS: Try to reduce redudancy (DRY Principle) in your code through the use of JS functions and variables.
+// 	💡 Use Chrome Dev Tools to view changes being made to the DOM
 
-		function renderPokemon() {
-			// your code here 
-		}
+// 	💡 Take a look at "styles.css" to see how the DOM Elements we're building out correspond to CSS class and ID selectors.
 
-	// ✅ Check Answer: 
-	// function init() {
-	// 	pokemon.forEach(renderPokemon);
-	// }
+// 	✨ BONUS: Try to reduce redudancy (DRY Principle) in your code through the use of JS functions and variables.
 
-	// init();
+// ✅ Check Answer:
+// function init() {
+// 	pokemon.forEach(renderPokemon);
+// }
+
+// init();
 
 // 🚧 Break Out Activity 2: Creating / Reading / Updating Elements
 
-  // 🚨 Comment out any conflicting code above before proceeding.
+// 🚨 Comment out any conflicting code above before proceeding.
 
-  // 1️⃣ Add the following capabilities to renderPokemon:
+// 1️⃣ Add the following capabilities to renderPokemon:
 
-	//  ✔️ Creates an "h3" element for each Pokemon and stores the element in a variable (pokeLikes)
-		// 	Use textContent() to fill in the heading with "Likes: "
-	
-	//  ✔️ Creates an "h5" element for each Pokemon and stores the element in a variable (likesNum)
-		// 	Assign likesNum a className of "likes-num"
-		// 	Use textContent() to fill in the heading with the Pokemon's number of likes
+function renderPokemon(pokemon) {
+	const pokeCard = document.createElement('div');
+	const pokeImg = document.createElement('img');
+	const pokeName = document.createElement('h3');
+	const pokeLikes = document.createElement('h3');
+	const likesNum = document.createElement('h5');
+	const likesBttn = document.createElement('button');
+	const deleteBttn = document.createElement('button');
 
-	//  ✔️ Creates a "button" element for each Pokemon and stores the element in a variable (likesBttn)
-		// 	Assign likesBttn a className of "likes-bttn"
-		// 	Use textContent() to fill in the button with a label, "♥"
+	pokeCard.id = `poke-${pokemon.id}`;
+	pokeCard.className = 'poke-card';
+	pokeImg.src = pokemon.img;
+	pokeImg.alt = pokemon.name + ' image';
+	pokeName.textContent = pokemon.name;
+	pokeLikes.textContent = 'Likes: ';
+	likesNum.className = 'likes-num';
+	likesNum.textContent = pokemon.likes;
+	likesBttn.className = 'likes-bttn';
+	likesBttn.textContent = '♥';
+	deleteBttn.className = 'delete-bttn';
+	deleteBttn.textContent = 'Delete';
 
-	//  ✔️ Creates a "button" element for each Pokemon and stores the element in a variable (deleteBttn)
-		// 	Assign deleteBttn a className of "delete-bttn"
-		// 	Use textContent() to fill in the button with a label, "Delete"
+	pokeCard.append(
+		pokeImg,
+		pokeName,
+		pokeLikes,
+		likesNum,
+		likesBttn,
+		deleteBttn
+	);
+	pokeContainer.append(pokeCard);
+}
 
-	//	✔️ Appends all variables to "pokeCard" before appending "pokeCard" to "pokeContainer"
-		// 	Use append() as necessary
+//  ✔️ Creates an "h3" element for each Pokemon and stores the element in a variable (pokeLikes)
+// 	Use textContent() to fill in the heading with "Likes: "
 
-	// 	💡 Use Chrome Dev Tools to view changes being made to the DOM
-	
-	// 	💡 Take a look at "styles.css" to see how the DOM Elements we're building out correspond to CSS class and ID selectors.
+//  ✔️ Creates an "h5" element for each Pokemon and stores the element in a variable (likesNum)
+// 	Assign likesNum a className of "likes-num"
+// 	Use textContent() to fill in the heading with the Pokemon's number of likes
 
-	// 	✨ BONUS: Try to reduce redudancy (DRY Principle) in your code through the use of JS functions and variables.
+//  ✔️ Creates a "button" element for each Pokemon and stores the element in a variable (likesBttn)
+// 	Assign likesBttn a className of "likes-bttn"
+// 	Use textContent() to fill in the button with a label, "♥"
 
-		function renderPokemon() {
-			// your code here 
-		}
+//  ✔️ Creates a "button" element for each Pokemon and stores the element in a variable (deleteBttn)
+// 	Assign deleteBttn a className of "delete-bttn"
+// 	Use textContent() to fill in the button with a label, "Delete"
 
-	// ✅ Check Answer: 
-	// function init() {
-	// 	pokemon.forEach(renderPokemon);
-	// }
+//	✔️ Appends all variables to "pokeCard" before appending "pokeCard" to "pokeContainer"
+// 	Use append() as necessary
 
-	// init();
+// 	💡 Use Chrome Dev Tools to view changes being made to the DOM
+
+// 	💡 Take a look at "styles.css" to see how the DOM Elements we're building out correspond to CSS class and ID selectors.
+
+// 	✨ BONUS: Try to reduce redudancy (DRY Principle) in your code through the use of JS functions and variables.
+
+// ✅ Check Answer:
+function init() {
+	pokemon.forEach(renderPokemon);
+}
+
+init();
